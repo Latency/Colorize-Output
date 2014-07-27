957c758f8fab6e8ea3cf5552c722834d97242661
Latency McLaughlin <latencyxxx@yahoo.com>
2014-07-26 23:20:21 -0700

    .

--- src/Windows/ColorizeOutputPackage.cs
+++ src/Windows/ColorizeOutputPackage.cs
@@ -18,7 +18,7 @@ namespace ColorizeOutput {
   [ProvideOptionPage(typeof (ColorizeOutputOptions), ColorizeOutputOptions.Category, ColorizeOutputOptions.SubCategory, 1000, 1001, true)]
   [ProvideProfile(typeof (ColorizeOutputOptions), ColorizeOutputOptions.Category, ColorizeOutputOptions.SubCategory, 1000, 1001, true)]
   [ProvideAutoLoad("adfc4e64-0397-11d1-9f4e-00a0c911004f")] //UICONTEXT_NoSolution; always loads 
-  [InstalledProductRegistration("ColorizeOutput", "Colorized debug and build window output messages - http://bio-hazard.cx/colorizeoutput", "1.4.5")]
+  [InstalledProductRegistration("ColorizeOutput", "Colorized debug and build window output messages - http://latency.github.io/Colorize-Output/", "1.4.5")]
   public class ColorizeOutputPackage : Package {
     protected override void Initialize() {
       TextManagerEvents.RegisterForTextManagerEvents();
--- src/Windows/OutputClassificationDefinitions.cs
+++ src/Windows/OutputClassificationDefinitions.cs
@@ -6,7 +6,6 @@
 
 #region
 
-using System;
 using System.ComponentModel.Composition;
 using System.Reflection;
 using System.Windows.Media;
@@ -15,12 +14,6 @@ using Microsoft.VisualStudio.Shell.Interop;
 using Microsoft.VisualStudio.Text.Classification;
 using Microsoft.VisualStudio.Utilities;
 using AssemblyInfo;
-
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Linq.Expressions;
 
 #endregion
 
